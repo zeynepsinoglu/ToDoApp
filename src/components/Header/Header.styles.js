c41a1564aa1c20a87687c36b176a8bad6d374956
zeynepsinoglu <zeynepsinoglu@zeynep-mbp.local>
2023-09-22 22:00:05 +0300

    first commit

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        padding:50,
+      },
+      baslık:{
+        flex:1,
+        fontSize:30,
+        fontWeight:'bold',
+        color:'#ffa500',
+      },
+      counter:{
+        fontSize:30,
+        color:'#ffa500',
+
+    },
+
+});
